4d66c8b39d172a4ca588b2c332ceb11b6caf8ea6
Avishek <avishekmukherje007@gmail.com>
2024-12-17 14:38:38 +0530

    Notes

---
+++ script.js
@@ -0,0 +1,128 @@
+// 1. Variables
+
+// let, var, const  ES6 JS let or var less use
+// const g = 'gdg'
+// let a = 9
+
+// var b = 9
+
+// 2. functions - a bunch of code which solves a task 
+    // 1. normal functions
+    // 2. arrow fns 
+// normal funtion
+// function bcrec(a,b){
+//     return a * b;
+// }
+// // main section
+// console.log(bcrec(3,5)) // prints the task 
+
+// let a = 10
+// let b = 5
+
+//  const add = (a,b) => {
+//     return a + b;
+//  }
+
+//  console.log(add(5,5))
+
+//  variable->function->object->task ka answer
+
+
+// 3. Conditional statements
+
+// let a = 10
+// let b = 2
+
+// if(a < b){
+//     console.log("I am smart");// agar true hai
+    
+// }else{
+//     console.log("I am stupid");// warna this one 
+    
+// }
+
+// 4. loops 
+// for loop
+// foreach loop
+
+// for(let i = 0; i<= 10;i++){
+//     console.log("GDG BCREC")
+// }
+
+// 5. arrays
+// const rollnos = [bcrec,'iem',4,5,6]
+
+// rollnos.forEach((rollnos) => {
+//     console.log(rollnos)
+// })
+
+// array functions
+// push() -> array k ander numbers/values dal deta hai
+// pop() -> last entered value to nikal denge
+// map() -> har ek value ko ek roll no assign karna
+// filter() -> boht saare data se imp data to tag karke sort
+
+// foreach -> ek array ka suppose 10 element hai mereko ek ek element targeted hisab se chahiye
+
+// const nos = [1,2,3,4,5]
+
+// nos.forEach((nos) => {
+//     console.log(nos)
+// })
+
+// API -> caterrer
+// HTTP req -> server -> pura data de dega le dhundle -> client k pas pura kabara a gaya -> actual usage ka data target karke nikalta hai 
+
+// DOM manipulation
+
+// hamlog javascript me bhet k HTML CSS ko control kar sakte hai using events ( click, hover, keypress) 
+
+// const para = document.getElementById('text')
+// const button = document.getElementById('change')
+
+// button.addEventListener('click', () => {
+//     para.textContent = "Badal diya"
+// });
+
+// Browser -> JS -> JSON -> Object
+
+// objects
+//  const student = {
+// //    name : Avishek
+//  }
+//     age: 20,
+//     backlogs: 5,
+//     marks : '0'
+// };
+
+// // object destructuring -> object se chiz ee nikalwana
+// console.log(student.name);
+// console.log(student.backlogs);
+// console.log(student.age);
+
+// JSON -> Javascript Object Notation , currency of web 
+
+// methods of json
+// 1. JSON.stringify() -> jitna bhi data aa rha hai sab ko string me convert kar do 
+
+// 2. JSON.parse() -> jitna data hai Object me badal dega
+
+
+
+
+// fetch -> JS V8 engine ka API handler hai
+
+// AngularJS , ReactJS, VueJs
+
+// axios -> fetch ka kami kya tha ? promise / async await ko isne respect nhi diya
+
+// Error Handling error aa bhi gaya website nhi dubayega
+// try{
+//     // exam pass karneka
+// }
+// catch{
+//     // koi bat nhi crash mat karna 
+// }
+// async await -> database boht dur rehta hai data ane me waqt lagta hai us time me kuch entertaiment 
+
+// console.log 
